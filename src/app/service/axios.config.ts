2b112d659337c714145e1a07071aca1b473a8dbd
AbuzCoder <abuzcoder@gmail.com>
2023-11-11 19:52:22 +0500

    update

--- src/app/service/axios.config.ts
+++ src/app/service/axios.config.ts
@@ -3,10 +3,7 @@ import axios from "axios";
 const http = axios.create({
   baseURL:
     process.env.NODE_ENV === "production"
-      ? "http://185.189.12.33/api/"
+      ? "/api/"
       : "http://localhost:8080/api/",
-  headers: {
-    "Content-Security-Policy": "upgrade-insecure-requests",
-  },
 });
 export default http;
