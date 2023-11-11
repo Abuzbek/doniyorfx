923b4878bf4a268fdc3ba22273bbb2c68648e77d
AbuzCoder <abuzcoder@gmail.com>
2023-11-11 20:55:52 +0500

    update

--- src/app/payment/CourseCard/index.tsx
+++ src/app/payment/CourseCard/index.tsx
@@ -5,7 +5,7 @@ type Props = {};
 const CourseCard = (props: Props) => {
   return (
     <div className={styles.course_card}>
-      <h3>Mobilografiya 1.0</h3>
+      <h3>Professional Mobilografiya</h3>
       <p>Kurs davomiyligi: 2 oy</p>
       <p>Start: 22-noyabr</p>
     </div>
