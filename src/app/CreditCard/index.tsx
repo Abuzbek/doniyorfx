17eeec2c043aabcc030d837db70a51ad93e484ce
AbuzCoder <abuzcoder@gmail.com>
2023-11-11 19:19:50 +0500

    update

---
+++ src/app/CreditCard/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from './styles.module.scss'
+type Props = {}
+
+const CreditCard = (props: Props) => {
+  return (
+    <div>
+        <div className={styles.credit_card}>
+
+        </div>
+    </div>
+  )
+}
+
+export default CreditCard
+
--- src/app/service/axios.config.ts
+++ src/app/service/axios.config.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const http = axios.create({
   baseURL:
     process.env.NODE_ENV === "production"
-      ? "/api/"
+      ? "http://185.189.12.33/api/"
       : "http://localhost:8080/api/",
 });
 export default http;
