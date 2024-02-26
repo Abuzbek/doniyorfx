d697579001242f223b9a845142a804d1dfba5d3c
Abuzbek <abuzcoder@gmail.com>
2024-02-26 11:17:47 +0500

    add smth

--- src/app/layouts/AppStudents/StudentsOpinion/opinion.data.ts
+++ src/app/layouts/AppStudents/StudentsOpinion/opinion.data.ts
@@ -2,6 +2,7 @@ import img1 from "@/assets/img/student-1.png";
 import img2 from "@/assets/img/student-2.png";
 import img3 from "@/assets/img/student-3.png";
 import img4 from "@/assets/img/student-4.png";
+import other from "@/assets/img/other-student.jpg";
 export const data = [
   {
     img:img1,
@@ -22,5 +23,10 @@ export const data = [
     img:img4,
     name: "Dilzoda",
     video: "https://www.youtube.com/watch?v=DlfugNf17og",
+  },
+  {
+    img:other,
+    name: "Boshqalar",
+    video: "https://youtu.be/0W8SxvL7cvI",
   },
 ];
