fe6962458fb427b613262304f3faacfd843017ba
Abuzbek <abuzcoder@gmail.com>
2024-05-23 04:41:55 +0500

    update

--- src/app/service/axios.config.ts
+++ src/app/service/axios.config.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const http = axios.create({
   baseURL:
     process.env.NODE_ENV === "production"
-      ? "https://doniyorfx.uz/api/"
+      ? "https://admin.doniyorfx.uz/api/"
       : "http://localhost:8080/api/",
 });
 export default http;
