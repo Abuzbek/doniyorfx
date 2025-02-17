bea7aa5173323d83ffee60a5ad52bc458fe36bf5
CORP\atojiboev <abdumannon.tojiboev@esynergy.lv>
2025-02-17 16:09:35 +0500

    chore: update axios.config.ts to use v2 API endpoint

--- src/app/service/axios.config.ts
+++ src/app/service/axios.config.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const http = axios.create({
   baseURL:
     process.env.NODE_ENV === "production"
-      ? "https://doniyorfx.uz/api/"
-      : "http://localhost:8080/api/",
+      ? "https://doniyorfx.uz/api/v2/"
+      : "http://localhost:8080/api/v2/",
 });
 export default http;
