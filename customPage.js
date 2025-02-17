cb4651517b4a8ebdfa729cbf0afca622e46a94bf
CORP\atojiboev <abdumannon.tojiboev@esynergy.lv>
2025-02-17 16:23:31 +0500

    Refactor customPage.js to use PaymentV2 model for Excel page

--- customPage.js
+++ customPage.js
@@ -1,28 +1,27 @@
 import { add } from "./componentLoader.js";
-import { Payments } from "./models/payments.model.js";
-import { Registered } from "./models/registered.model.js";
+import { PaymentV2s } from "./models/payment-V2.model.js";
 
 export const pages = {
   excel: {
     component: add("components/custom-page.jsx", "Excel"),
     icon: "File",
     handler: async (request, response, context) => {
-      const payments = await Payments.find({});
+      const payments = await PaymentV2s.find({});
       return {
         payments,
       };
     },
   },
-  registeredExcel: {
-    component: add("components/registered-page.jsx", "RegisteredExcel"),
-    icon: "User",
-    handler: async (request, response, context) => {
-      const payments = await Registered.find({});
-      return {
-        payments,
-      };
-    },
-  },
+  // registeredExcel: {
+  //   component: add("components/registered-page.jsx", "RegisteredExcel"),
+  //   icon: "User",
+  //   handler: async (request, response, context) => {
+  //     const payments = await Registered.find({});
+  //     return {
+  //       payments,
+  //     };
+  //   },
+  // },
   //   designSystemExamples: {
   //     component: DESIGN_SYSTEM_PAGE,
   //     icon: "Layout",
